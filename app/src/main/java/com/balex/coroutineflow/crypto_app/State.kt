e99bc75ab3f55c148c8bf4a39b271692a32d3b83
balex <balexvicx@gmail.com>
2024-04-13 01:27:14 +0200

    asLiveData

--- app/src/main/java/com/balex/coroutineflow/crypto_app/State.kt
+++ app/src/main/java/com/balex/coroutineflow/crypto_app/State.kt
@@ -1,7 +1,7 @@
 package com.balex.coroutineflow.crypto_app
 
 sealed class State {
-    object Initial : State()
-    object Loading : State()
+    data object Initial : State()
+    data object Loading : State()
     data class Content(val currencyList: List<Currency>) : State()
 }
