a436c9de6dbd7556436f40037637b33e1d6232d9
balex <balexvicx@gmail.com>
2024-04-13 01:56:23 +0200

    asLiveData part 2

--- app/src/main/java/com/balex/coroutineflow/crypto_app/CryptoViewModel.kt
+++ app/src/main/java/com/balex/coroutineflow/crypto_app/CryptoViewModel.kt
@@ -1,11 +1,12 @@
 package com.balex.coroutineflow.crypto_app
 
+import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 
@@ -14,23 +15,18 @@ class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-    private val _state = MutableLiveData<State>(State.Initial)
-    val state: LiveData<State> = _state
-
-    init {
-        loadData()
-    }
-
-    private fun loadData() {
-        repository.getCurrencyList()
-            .onStart {
-                val currentState = _state.value
-                if (currentState !is State.Content || currentState.currencyList.isEmpty()) {
-                    _state.value = State.Loading
-                }
-            }
-            .filter { it.isNotEmpty() }
-            .onEach { _state.value = State.Content(currencyList = it) }
-            .launchIn(viewModelScope)
-    }
+    val state: LiveData<State> = repository.getCurrencyList()
+        .filter { it.isNotEmpty() }
+        .map { State.Content(currencyList = it) as State }
+        .onStart {
+            Log.d("CryptoViewModel", "Started")
+            emit(State.Loading)
+        }
+        .onEach {
+            Log.d("CryptoViewModel", "OnEach")
+        }
+        .onCompletion {
+            Log.d("CryptoViewModel", "Complete")
+        }
+        .asLiveData()
 }
